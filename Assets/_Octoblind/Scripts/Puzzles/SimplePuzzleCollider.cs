4aa04920de66455eccada58dbc8c3929da06a536
TimeNull <65688039+TimeNull@users.noreply.github.com>
2021-05-03 02:08:37 -0300

    simplepuzzlecollider

--- Assets/_Octoblind/Scripts/Puzzles/SimplePuzzleCollider.cs
+++ Assets/_Octoblind/Scripts/Puzzles/SimplePuzzleCollider.cs
@@ -8,18 +8,17 @@ public class SimplePuzzleCollider : MonoBehaviour
     private int stepOnConllision;
     public Puzzle puzzle;
     public bool canCollide;
+    public string tagToCheck;
     [SerializeField]
     private string correctObject;
     public string correctTool;
     public int stepOnTool;
-
-    
+    public bool talker;
     private void Start()
     {
-        
         //-----LOG ERROR-----
         if (!this.CompareTag("PuzzleCollider")) Debug.LogError("A tag deste collider não é 'Puzzle Collider'", this);
-
+        if (!this.GetComponent<Collider>().isTrigger) Debug.LogError("Esse collider não é trigger", this);
         if(GetComponentInParent<Puzzle>() != null)
         {
             puzzle = GetComponentInParent<Puzzle>();
@@ -34,18 +33,18 @@ public class SimplePuzzleCollider : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log(other.tag);
         if (canCollide)
         {
-            if (other.CompareTag("Trigger"))
+            if (other.CompareTag(tagToCheck))
             {
 
                 if (other.GetComponentInParent<objectController>()?.type == correctObject)
                 {
+                    //Debug.Log(other.transform.parent.gameObject);
                     puzzle.AddPiece(other.transform.parent.gameObject);
                     puzzle.AchiveStep(stepOnConllision, true);
                 }
-
-
             }
         }        
     }
@@ -53,7 +52,7 @@ public class SimplePuzzleCollider : MonoBehaviour
     {
         if (canCollide)
         {
-            if (other.CompareTag("Trigger"))
+            if (other.CompareTag(tagToCheck))
             {
                 if (other.GetComponentInParent<objectController>()?.type == correctObject)
                 {
@@ -68,5 +67,9 @@ public class SimplePuzzleCollider : MonoBehaviour
         //Aqui estou dizendo que ele só pode ativar com uma tool depois que colidiu, imagino que esse não seja o caso para todo puzzle mas não farei esse modificação sem necessidade
         //Não gosto muito disso, esse código é muito específico e deveria estar no Puzzle não no collider.
         if (puzzle.CheckStep(stepOnConllision) && !puzzle.CheckStep(stepOnTool)) puzzle.AchiveStep(stepOnTool, true);
+    }
+    public void AchiveTalkStep()
+    {
+        puzzle.AchiveStep(stepOnTool,true);
     }
 }
